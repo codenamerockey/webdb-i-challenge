7a996eb7f4fd5e142acf65b6735946a5e53a7a15
Rockey Simmons <codenamerockey@gmail.com>
2019-10-07 15:41:26 -0700

    completed http methods for challenge, all are in working condition performing full CRUD operations using knex

--- server.js
+++ server.js
@@ -6,20 +6,85 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  res.send('Web DB Challenge 1 server');
+server.get('/api/accounts', (req, res) => {
+  db.select('*')
+    .from('accounts')
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message:
+          'There is a problem retrieving the account information from the database'
+      });
+    });
 });
 
-server.post('/', (req, res) => {
-  res.send('Web DB Challenge 1 server');
+server.get('/api/accounts/:id', (req, res) => {
+  db.select('*')
+    .from('accounts')
+    .where('id', '=', req.params.id)
+    .first()
+    .then(account => {
+      res.status(200).json(account);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message:
+          'There was a problem retrieving the requested account from the database'
+      });
+    });
 });
 
-server.put('/', (req, res) => {
-  res.send('Web DB Challenge 1 server');
+server.post('/api/accounts', (req, res) => {
+  const postData = req.body;
+  if (!postData.name || !postData.budget) {
+    res
+      .status(400)
+      .json({ message: 'Please include a name and a budget amount' });
+  }
+
+  db('accounts')
+    .insert(postData, 'id')
+    .then(ids => {
+      console.log(ids); // returns an array of id's or an array of 1 id if posting only one object.
+      res.status(201).json(ids);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Problem creating account in database' });
+    });
 });
 
-server.delete('/', (req, res) => {
-  res.send('Web DB Challenge 1 server');
+server.put('/api/accounts/:id', (req, res) => {
+  const putID = req.params.id;
+  const changes = req.body;
+
+  db('accounts')
+    .where({ id: putID })
+    .update({ budget: changes.budget })
+    .then(updatedRows => {
+      res.status(200).json(updatedRows);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: 'Problem saving the updated account to database' });
+    });
+});
+
+server.delete('/api/accounts/:id', (req, res) => {
+  const delID = req.params.id;
+  db('accounts')
+    .where({ id: delID })
+    .del()
+    .then(deletedRows => {
+      res.status(200).json(deletedRows);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Problem updating the database with your delete request'
+      });
+    });
 });
 
 module.exports = server;
